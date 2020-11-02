5d61af77b22079df19d74f31215fda776e9a9d62
Carlos Britos <clscrea@gmail.com>
2020-11-02 02:43:14 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
   url: 'http://localhost:5000/firestore-grafica-1c808/us-central1',
   firebase: {
-    apiKey: 'AIzaSyCUW7OAbKZQxD5HB2tCrj3Qt5xQ0ABgqeI',
-    authDomain: 'firestore-grafica-1c808.firebaseapp.com',
-    databaseURL: 'https://firestore-grafica-1c808.firebaseio.com',
-    projectId: 'firestore-grafica-1c808',
-    storageBucket: 'firestore-grafica-1c808.appspot.com',
-    messagingSenderId: '570335554829',
-    appId: '1:570335554829:web:b9dcf0bf8606c326f2d444'
+    apiKey: 'your key',
+    authDomain: 'your domain',
+    databaseURL: 'your database url',
+    projectId: 'your id',
+    storageBucket: 'your bucket',
+    messagingSenderId: 'your sender id',
+    appId: 'your app id'
   }
   
 };
